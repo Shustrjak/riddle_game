ae55d1d2aa6a52987a774db37463a62b67bbfcd9
Alexander Rudenko <rudenko.alex.ol@gmail.com>
2021-10-12 22:40:25 +0300

    fix(script.js) изменение положения генератора загаданного числа

--- script.js
+++ script.js
@@ -1,16 +1,13 @@
 'use strict';
 
-
-let rand = function rand() {
-    return Math.floor(Math.random() * 100) + 1;
-};
-// const isNumber = function (num) {
-//     return !isNaN(parseFloat(num)) && isFinite(num) && num !== null;
-// };
-function riddle (number){
-    const hiddenNumber = number;
-    console.log('hiddenNumber: ', hiddenNumber);  
+function riddle (){
+    let rand = function() {
+        return Math.floor(Math.random() * 100) + 1;
+    };
+    console.log('hiddenNumber: ', rand);
+    
     function hideNumber(number) {
+        
         const num = prompt('Угадайте число от 1 до 100');
         if (num > number) {
             alert('Загаданное число меньше');
@@ -18,7 +15,7 @@ function riddle (number){
             alert('Загаданное число больше');
         } else if (num == number) {
             alert('Вы угадали');
-            let conf = confirm('Сыграем еще?')
+            let conf = confirm('Сыграем еще?');
             if (conf == false) {
                 alert('Игра окончена');
                 window.close();
@@ -32,8 +29,8 @@ function riddle (number){
         
         hideNumber(number);
     }
-    hideNumber(number);
+    hideNumber(rand());
     
 }
 
-riddle(rand());
+riddle();
