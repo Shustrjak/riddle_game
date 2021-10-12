0d4119b549b8c91053691dadeb4c563c1c72d8ce
Alexander Rudenko <rudenko.alex.ol@gmail.com>
2021-10-12 21:14:37 +0300

    fix(script.js) refactor function

--- script.js
+++ script.js
@@ -4,31 +4,35 @@
 let rand = function rand() {
     return Math.floor(Math.random() * 100) + 1;
 };
-const isNumber = function (num) {
-    return !isNaN(parseFloat(num)) && isFinite(num) && num !== null;
-};
+// const isNumber = function (num) {
+//     return !isNaN(parseFloat(num)) && isFinite(num) && num !== null;
+// };
 function riddle (number){
     const hiddenNumber = number;
     console.log('hiddenNumber: ', hiddenNumber);
     function hideNumber(number) {
         const num = prompt('Угадайте число от 1 до 100');
-
+        
         if (num > number) {
             alert('Загаданное число меньше');
             hideNumber(number);
-        }
-        if (num < number) {
+        }else if (num < number) {
             alert('Загаданное число больше');
             hideNumber(number);
-
-        }
-        if (num == number) {
+        } else if (num == number) {
             alert('Вы угадали');
-            return confirm('Сыграем еще?');
+            if (confirm('Сыграем еще?') == true) {
+                hideNumber(number);
+            } else {
+                window.close();
+            }
+        } else {
+            alert('введите число!');
         }
         hideNumber(hiddenNumber);
     }
     hideNumber(number);
+    
 }
 
 riddle(rand());
