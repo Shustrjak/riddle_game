ff88bf79dad31cdef6c093d7f2cc4689ebc0a520
Alexander Rudenko <rudenko.alex.ol@gmail.com>
2021-10-12 20:44:35 +0300

    fix(script.js): правка рекурсии

--- script.js
+++ script.js
@@ -4,74 +4,31 @@
 let rand = function rand() {
     return Math.floor(Math.random() * 100) + 1;
 };
-console.log('rand: ', rand());
+const isNumber = function (num) {
+    return !isNaN(parseFloat(num)) && isFinite(num) && num !== null;
+};
+function riddle (number){
+    const hiddenNumber = number;
+    console.log('hiddenNumber: ', hiddenNumber);
+    function hideNumber(number) {
+        const num = prompt('Угадайте число от 1 до 100');
 
+        if (num > number) {
+            alert('Загаданное число меньше');
+            hideNumber(number);
+        }
+        if (num < number) {
+            alert('Загаданное число больше');
+            hideNumber(number);
 
-
-
-function hiddenNumber(number) {
-    const num = parseInt(prompt('Угадайте число от 1 до 100'));
-    while (num > number) {
-        alert('Загаданное число меньше');
+        }
+        if (num == number) {
+            alert('Вы угадали');
+            return confirm('Сыграем еще?');
+        }
+        hideNumber(hiddenNumber);
     }
-    while (num < number) {
-        alert('Загаданное число больше');
-    }
-    while (num == number) {
-        alert('Вы угадали');
-        return;
-    }
-    
-    
+    hideNumber(number);
 }
-hiddenNumber(rand());
 
-
-
-
-
-
-
-
-
-
-// const title = confirm("Угадай число от 1 до 100");
-// function cancelParams(title) {
-//     if (title == false) {
-//         window.close();
-//     } else{
-//     let rand = function rand() {
-//         return Math.floor(Math.random() * 100) + 1;
-//     };
-//     console.log('rand:', rand());
-// }
-
-// }
-
-
-// function isNumber(num){
-//     return !isNaN(parseInt(num)) && isFinite(num) && num !== null;
-// }
-// let hiddenNumber = parseInt(prompt('Напиши загаданное число?'));
-
-// function getNumber(num) {
-//     if (!isNumber(num) === !isNumber(rand())) {
-//         return console.log('вы угадали');
-//     } else{
-//         return parseInt(prompt('Напиши загаданное число?'));
-//     }
-// }
-
-// getNumber(hiddenNumber);
-
-
-
-// функцияОбертка() {
-//     return функцияРекурсии {
-//         какой то код
-//         функцияРекурсии(); // это рекурсия
-//     }
-// }
-
-// const переменная = функцияОбертка(); //это замыкание
-// переменная(передаемВЗамыканиеЧисло);
+riddle(rand());
